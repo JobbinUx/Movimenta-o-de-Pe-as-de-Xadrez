994d955b17b7a00e7279a6ca56babf04447dc112
JobbinUx <joao28dark@gmail.com>
2025-09-04 13:11:04 +0000

    Realizado criação do Bispo e Rainha

--- Desafionovato.c
+++ Desafionovato.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 
-int casasTorre = 5
-printf("Movimento da Torre:\n");
-for (int i = 1; i <= casasTorre; i++){
-printf("Direita\n");
+int main() {
 
+    int casasTorre = 5;
+    printf("Movimento da Torre:\n");
+
+    for (int i = 1; i <= casasTorre; i++) {
+    printf("Direita\n");
+
+    }
+
+    int casasBispo = 5;
+    int i = 1;
+    printf("\nMovimento do Bispo:\n");
+
+    while (i <= casasBispo) {
+    printf("Cima, Direita\n");
+    i++;
+
+    }
+
+    int casasRainha = 8;
+    int j = 1;
+    printf("\nMovimento da Rainha:\n");
+    do {
+        
+    printf("Esquerda\n");
+    j++;
+    } while (j <= casasRainha);
+
+    return 0;
 }
